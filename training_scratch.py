b2b3e5ab746a13a87fe01fccdcf6bfae6917feae
Ruben-MR <32171451+Ruben-MR@users.noreply.github.com>
2022-03-29 13:48:06 +0200

    Modifying new dataset classes and adding them to training_scratch.py

--- training_scratch.py
+++ training_scratch.py
@@ -4,6 +4,7 @@ from utils.train_utils import PreProcessOptions, RescalerOptions
 from utils.inference_utils import EventPreprocessor, IntensityRescaler
 from utils.train_utils import plot_training_data, pad_all, loss_fn, training_loop
 from utils.loading_utils import get_device
+from utils.ecoco_dataset import ECOCO_Train_Dataset, ECOCO_Validation_Dataset
 import lpips
 
 if __name__ == "__main__":
@@ -22,24 +23,23 @@ if __name__ == "__main__":
     # ignore the code above, they are just used for taking out the event tensor and model
     device = get_device(True)
     # DATA_DIR = '/home/richard/Q3/Deep_Learning/ruben-mr.github.io/data'
-    batch_size = 2
-    sequence_length = 5
-    events = torch.tensor(full_event_tensor(range(batch_size), sequence_length, DATA_DIR)[0], dtype=torch.float64).cuda().float()
-    images = torch.tensor(full_image_tensor(range(batch_size), sequence_length, DATA_DIR)[0], dtype=torch.float64).cuda().float()
+    seq_length = 3
+    start_idx = 8
+    data_path = DATA_DIR
+    sequence_index = 42
 
-    # =============================
-    # data pre-processing
-    events, images = pad_all(model, events, images)
-    # =============================
-    train_loader = [(events, images)]
-    validation_loader = [(events, images)]
+    train_dataset = ECOCO_Train_Dataset(sequence_length=seq_length, start_index=start_idx, path=data_path)
+    val_dataset = ECOCO_Validation_Dataset(sequence_length=seq_length, start_index=start_idx, path=data_path)
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=2, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(train_dataset, batch_size=2, shuffle=True)
 
     if torch.cuda.is_available():
         reconstruction_loss_fn = lpips.LPIPS(net='vgg').cuda()
     else:
         reconstruction_loss_fn = lpips.LPIPS(net='vgg')
 
-    train_losses, val_losses = training_loop(model, loss_fn, train_loader, validation_loader, reconstruction_loss_fn, epoch=5)
+    train_losses, val_losses = training_loop(model, loss_fn, train_loader, val_loader, reconstruction_loss_fn, epoch=2)
     print(train_losses)
     print(val_losses)
     plot_training_data(train_losses, val_losses)
--- utils/ecoco_dataset.py
+++ utils/ecoco_dataset.py
@@ -6,7 +6,7 @@ from config import DATA_DIR
 from torchvision.io import read_image
 
 
-class ECOCO_Dataset(torch.utils.data.Dataset):
+class ECOCO_Train_Dataset(torch.utils.data.Dataset):
 
     def __init__(self, start_index=0, sequence_length=8, path=DATA_DIR):
         """
@@ -16,9 +16,7 @@ class ECOCO_Dataset(torch.utils.data.Dataset):
         images in the video sequence. This essentially means that the produced sequences will contain
         (sequence_length + 1) images, and the corresponding transition tensors (both flow, and event) in between those
         images (so, an amount equal to sequence_length, each).
-
         :param start_index: the first index from which to retrieve the sequence.
-
         :param path: the path of the directory within which the dataset directory can be found.
 
         As an example, with start_index=8 and sequence_length=3,
@@ -27,9 +25,8 @@ class ECOCO_Dataset(torch.utils.data.Dataset):
             - frame: [8, 9, 10, 11]
             - flow: [9, 10, 11]
         (differences in the indices per type of tensor are due to the way the database indexing is done)
-
         """
-        assert start_index <= 50
+        assert start_index + sequence_length <= 50
 
         self.sequence_length = sequence_length
         self.start_idx = start_index
@@ -41,21 +38,18 @@ class ECOCO_Dataset(torch.utils.data.Dataset):
         :param idx: the index corresponding to the desired sequence to read from.
         :return:
             - events, a torch.Tensor of shape [self.sequence_length, 5, 180, 240]
-            - frames, a torch.Tensor of shape [(self.sequence_length + 1), 180, 240]
+            - frames, a torch.Tensor of shape [(self.sequence_length + 1), 1, 180, 240]
             - flows, a torch.Tensor of shape [self.sequence_length, 2, 180, 240]
         """
-        assert idx <= 1000
+        assert idx < 950
 
-        if idx < 950:
-            sequence_path = os.path.join(self.root_path,
-                                         "ecoco_depthmaps_test",
-                                         "train",
-                                         "sequence_{:>010d}".format(idx))
-        else:
-            sequence_path = os.path.join(self.root_path,
-                                         "ecoco_depthmaps_test",
-                                         "validation",
-                                         "sequence_{:>010d}".format(idx))
+        while idx in [107, 382]:
+            idx = int(np.random.randint(0, 950, (1,)))
+
+        sequence_path = os.path.join(self.root_path,
+                                     "ecoco_depthmaps_test",
+                                     "train",
+                                     "sequence_{:>010d}".format(idx))
         # print(f"{sequence_path=}")
 
         flow_path = os.path.join(sequence_path, "flow")
@@ -65,12 +59,12 @@ class ECOCO_Dataset(torch.utils.data.Dataset):
         # print(f"{frame_path=}")
         # print(f"{event_path=}")
 
-        frames = torch.zeros(size=[self.sequence_length + 1, 180, 240])
+        frames = torch.zeros(size=[self.sequence_length + 1, 1, 180, 240])
         flows = torch.zeros(size=[self.sequence_length, 2, 180, 240])
         events = torch.zeros(size=[self.sequence_length, 5, 180, 240])
 
         for i, sequence_index in enumerate(range(self.start_idx, self.start_idx + self.sequence_length)):
-            print(f"{i, sequence_index=}")
+            # print(f"{i, sequence_index=}")
             flow = np.load(os.path.join(flow_path, "disp01_{:>010d}.npy".format(sequence_index+1)))
             event = np.load(os.path.join(event_path, "event_tensor_{:>010d}.npy".format(sequence_index)))
             frame = read_image(os.path.join(frame_path, "frame_{:>010d}.png".format(sequence_index)))
@@ -85,13 +79,85 @@ class ECOCO_Dataset(torch.utils.data.Dataset):
         # print(f"{self.sequence_length, self.start_idx + self.sequence_length=}")
         last_frame = read_image(os.path.join(frame_path,
                                              "frame_{:>010d}.png".format(self.start_idx + self.sequence_length)))
-        print(f"{type(last_frame), last_frame.shape=}")
+        # print(f"{type(last_frame), last_frame.shape=}")
         frames[self.sequence_length] = last_frame
 
         return events, frames, flows
 
     def __len__(self):
-        raise NotImplementedError
+        return 950
+
+
+class ECOCO_Validation_Dataset(torch.utils.data.Dataset):
+
+    def __init__(self, start_index=0, sequence_length=8, path=DATA_DIR):
+        """
+        Dataset class for instantiating individual elements of the event dataset.
+
+        :param sequence_length: the length of the video sequence. By this we mean the number of transition steps between
+        images in the video sequence. This essentially means that the produced sequences will contain
+        (sequence_length + 1) images, and the corresponding transition tensors (both flow, and event) in between those
+        images (so, an amount equal to sequence_length, each).
+        :param start_index: the first index from which to retrieve the sequence.
+        :param path: the path of the directory within which the dataset directory can be found.
+        """
+        assert start_index + sequence_length <= 50
+
+        self.sequence_length = sequence_length
+        self.start_idx = start_index
+        self.root_path = path
+
+    def __getitem__(self, idx):
+        """
+        Method for retrieving the relevant tensors of sequence specified by idx
+        :param idx: the index corresponding to the desired sequence to read from.
+        :return:
+            - events, a torch.Tensor of shape [self.sequence_length, 5, 180, 240]
+            - frames, a torch.Tensor of shape [(self.sequence_length + 1), 180, 240]
+            - flows, a torch.Tensor of shape [self.sequence_length, 2, 180, 240]
+        """
+        assert idx < 50
+        idx += 950
+        sequence_path = os.path.join(self.root_path,
+                                     "ecoco_depthmaps_test",
+                                     "validation",
+                                     "sequence_{:>010d}".format(idx))
+        # print(f"{sequence_path=}")
+
+        flow_path = os.path.join(sequence_path, "flow")
+        frame_path = os.path.join(sequence_path, "frames")
+        event_path = os.path.join(sequence_path, "VoxelGrid-betweenframes-5")
+        # print(f"{flow_path=}")
+        # print(f"{frame_path=}")
+        # print(f"{event_path=}")
+
+        frames = torch.zeros(size=[self.sequence_length + 1, 1, 180, 240])
+        flows = torch.zeros(size=[self.sequence_length, 2, 180, 240])
+        events = torch.zeros(size=[self.sequence_length, 5, 180, 240])
+
+        for i, sequence_index in enumerate(range(self.start_idx, self.start_idx + self.sequence_length)):
+            # print(f"{i, sequence_index=}")
+            flow = np.load(os.path.join(flow_path, "disp01_{:>010d}.npy".format(sequence_index+1)))
+            event = np.load(os.path.join(event_path, "event_tensor_{:>010d}.npy".format(sequence_index)))
+            frame = read_image(os.path.join(frame_path, "frame_{:>010d}.png".format(sequence_index)))
+
+            # print(f"{type(flow), flow.shape=}")
+            # print(f"{type(event), event.shape=}")
+            # print(f"{type(frame), frame.shape=}")
+            flows[i] = torch.from_numpy(flow)
+            events[i] = torch.from_numpy(event)
+            frames[i] = frame
+
+        # print(f"{self.sequence_length, self.start_idx + self.sequence_length=}")
+        last_frame = read_image(os.path.join(frame_path,
+                                             "frame_{:>010d}.png".format(self.start_idx + self.sequence_length)))
+        # print(f"{type(last_frame), last_frame.shape=}")
+        frames[self.sequence_length] = last_frame
+
+        return events, frames, flows
+
+    def __len__(self):
+        return 50
 
 
 if __name__ == '__main__':
@@ -101,17 +167,25 @@ if __name__ == '__main__':
     data_path = DATA_DIR
     sequence_index = 42
 
-    dataset = ECOCO_Dataset(sequence_length=seq_length, start_index=start_idx, path=data_path)
+    train_dataset = ECOCO_Train_Dataset(sequence_length=seq_length, start_index=start_idx, path=data_path)
+    val_dataset = ECOCO_Validation_Dataset(sequence_length=seq_length, start_index=start_idx, path=data_path)
 
-    # print(f"{dataset.sequence_length=}")
-    # print(f"{dataset.start_idx=}")
-    # print(f"{dataset.root_path=}")
+    #print(f"{dataset.sequence_length=}")
+    #print(f"{dataset.start_idx=}")
+    #print(f"{dataset.root_path=}")
 
-    event_tensor, frame_tensor, flow_tensor = dataset.__getitem__(sequence_index)
+    event_tensor, frame_tensor, flow_tensor = train_dataset.__getitem__(sequence_index)
 
     print(f"{type(event_tensor), event_tensor.shape=}")
     print(f"{type(frame_tensor), frame_tensor.shape=}")
     print(f"{type(flow_tensor), flow_tensor.shape=}")
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=2, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(train_dataset, batch_size=2, shuffle=True)
+
+    for events, frames, flows in val_loader:
+        assert events.shape == torch.Size([2, 3, 5, 180, 240]) and frames.shape == torch.Size([2, 4, 1, 180, 240]) \
+               and flows.shape == torch.Size([2, 3, 2, 180, 240])
 
     # print("\n\nFrames")
     # for i, image in enumerate(frame_tensor):
