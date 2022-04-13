7c41e49b795a738d71db6037e25406d1f7d9a68a
PFery4 <paulfery4@gmail.com>
2022-04-13 18:06:38 +0200

    uploading short_video script (updated version)

--- utils/short_video.py
+++ utils/short_video.py
@@ -1,47 +1,38 @@
 import glob
 import os.path
 import cv2
-from config import DATA_DIR
+from config import DATA_DIR, SAVED_DIR
 
-name = "saved_07-04-2022_09-05"
-filename = os.path.join(DATA_DIR, "results", name)
 
-print(filename)
-#
-# with open(filename, 'r+') as fp:
-#     lines = fp.readlines()
-#     indices = [0]
-#     indices.extend(list(range(50000, 10050000)))
-#     lines = [lines[i] for i in indices]
-#
-#     print("lines done")
-#
-#     fp.seek(0)
-#     fp.truncate()
-#     fp.writelines(lines)
-#
-#     print(f"truncating done")
+def make_video(name):
+    filename = os.path.join(DATA_DIR, "results", name)
 
-# img *= 255
-#
-#
-# cv2.imshow("", img)
-# cv2.waitKey()
+    print(filename)
 
-os.chdir(filename)
+    os.chdir(filename)
 
-video_file = f'{name}.mp4'
-image_size = (240, 180)
-fps = 15
+    video_file = f'{name}.mp4'
+    image_size = (240, 180)
+    fps = 15
 
-out = cv2.VideoWriter(video_file, cv2.VideoWriter_fourcc(*'mp4v'), fps, image_size)
+    out = cv2.VideoWriter(video_file, cv2.VideoWriter_fourcc(*'mp4v'), fps, image_size)
 
-for file in sorted(glob.glob("*.png")):
-    print(file)
+    for file in sorted(glob.glob("*.png")):
+        print(file)
 
-    img = cv2.imread(file)
-    cv2.imshow('', img)
-    cv2.waitKey(1)
-    out.write(img)
+        img = cv2.imread(file)
+        cv2.imshow('', img)
+        cv2.waitKey(1)
+        out.write(img)
 
-out.release()
+    out.release()
+
+
+if __name__ == '__main__':
+
+    names = os.listdir(SAVED_DIR)
+    names.remove("unused")
+
+    for name in names:
+        make_video(name)
+
